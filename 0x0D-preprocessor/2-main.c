6127cbcd0b75c95430811764909e38c831deba20
FellyHoscky <sweetnatureug@gmail.com>
2022-04-13 05:31:04 -0700

    The program to print the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * The program to print the name of the file thar it had compiled
+ *Return success
+ *
+ */
+ 
+ int main(void)
+ {
+ 	printf("%s\n", __FILE__);
+	return (0);
+}
